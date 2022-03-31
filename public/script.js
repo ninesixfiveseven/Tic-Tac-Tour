648fdb673cf2dfddea6074130f49d45b53a3ea73
itsabank <102414990+itsabank@users.noreply.github.com>
2022-03-31 23:11:03 +0700

    Modal Login & Signup

--- public/script.js
+++ public/script.js
@@ -1,23 +1,36 @@
-var span = document.getElementsByClassName("close")[0];
-
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modalLogin.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modalLogin) {
-    modalLogin.style.display = "none";
-  }
-}
-
 const modalLogin = document.querySelector("#modalLogin");
 const btnLogin = document.querySelector("#btnLogin");
 btnLogin.addEventListener("click", showModalLogin);
 
 function showModalLogin(event) {
-    console.log("1")
     modalLogin.style.display = "flex";
+}
+
+const closeModalLogin = document.querySelector("#modalLogin .close");
+
+closeModalLogin.onclick = function() {
+    modalLogin.style.display = "none";
+}
+
+const modalSignup = document.querySelector("#modalSignup");
+const btnSignup = document.querySelector("#btnSignup");
+btnSignup.addEventListener("click", showModalSignup);
+
+function showModalSignup(event) {
+    modalSignup.style.display = "flex";
+}
+
+const closemodalSignup = document.querySelector("#modalSignup .close");
+
+closemodalSignup.onclick = function() {
+    modalSignup.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modalLogin) {
+      modalLogin.style.display = "none";
+    } else if (event.target == modalSignup) {
+        modalSignup.style.display = "none";
+    }
 }
